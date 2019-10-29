117e1da2a01d06cac72cee53f380641bc98ad7a9
Howle <mdv413@mail.ru>
2019-10-29 15:37:45 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,21 @@
+﻿package main
+import (
+	"github.com/gorilla/mux"
+	"time"
+	"net/http"
+	"io"
+	"fmt"
+)
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	io.WriteString(w, `{"alive": true}`)
+	fmt.Printf("Have connection\n")
+	time.Sleep(500 * time.Millisecond)
+}
+
+func main() {
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+	http.ListenAndServe(":8080", r)
+}
